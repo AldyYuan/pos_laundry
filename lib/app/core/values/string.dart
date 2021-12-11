272c704b7a3fee5e53add7121a33e3a0c30b4f76
Aldy Yuan <yuanaldy@gmail.com>
2021-12-11 21:50:25 +0700

    Add firebase

--- lib/app/core/values/string.dart
+++ lib/app/core/values/string.dart
@@ -6,3 +6,7 @@ const String baseUrl = 'https://android-test-fljnd6wana-as.a.run.app/';
 const String contentType = 'application/x-www-form-urlencoded';
 const String jwtToken =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJxIjoiIiwibGltaXQiOjEwMCwib2Zmc2V0IjowfQ.fbppcH8U1X8hTbA0Ka1atVokLwTivfIGOToFjMvIIHc';
+
+// Collection Firestore
+const String cTransaction = 'Transaksi';
+const String cLayanan = 'Layanan';
